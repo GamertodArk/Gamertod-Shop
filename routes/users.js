dc2375959d287a678a306b3a22f0cd3f57b4952d
Elvis Garcia <cavesagarcia@hotmail.com>
2018-08-24 22:05:33 -0430

    Working on Signup View

--- routes/users.js
+++ routes/users.js
@@ -4,4 +4,9 @@ router.get('/login', (req, res) => {
 	res.render('login', {title: 'Iniciar sesion'});
 });
 
+router.get('/signup', (req, res) => {
+	res.render('signup', {title: 'Registrate'});
+	// res.send('Hello world');
+});
+
 module.exports = router;
