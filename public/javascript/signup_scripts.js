5815f671199093e4d14ec4a73f1fb7773aa534b2
Elvis Garcia <cavesagarcia@hotmail.com>
2018-08-31 21:53:51 -0430

    Signup message structure built

--- public/javascript/signup_scripts.js
+++ public/javascript/signup_scripts.js
@@ -1,5 +1,12 @@
 let btn = __('signup_btn');
 
+function generate_message() {
+	let div = document.createElement('div');
+	let p = document.createElement('p');
+
+	div.appendChild(p);
+}
+
 btn.addEventListener('click', (event) => {
 	event.preventDefault();
 
@@ -16,41 +23,27 @@ btn.addEventListener('click', (event) => {
 	let username = __('username').value;
 	let last_name = __('last_name').value;
 
-	let user_data = {
-		name,
-		email,
-		pass1,
-		pass2,
-		country,
-		username,
-		last_name
-		// b_day,
-		// b_month,
-		// b_year
-	}
 
-	// for(item in user_data) {
-
-	// }
-
+	// Remove the error class from the labels
 	let labels = document.getElementsByTagName('label');
 	for (var i = 0; i < labels.length; i++) {
 		labels[i].classList.remove('label-error');
 	}
 
-	// console.log(user_data);
 	
+	// Set all the values of the form in a query que send it to the server with ajax
 	let params = `name=${name}&last_name=${last_name}&email=${email}&username=${username}&country=${country}&gender=${gender}&password=${pass1}&password2=${pass2}&b_day=${b_day}&b_month=${b_month}&b_year=${b_year}`;
 	sendAjax('/users/signup', 'POST', params, res => {
 		let data = JSON.parse(res);
 
 		if (data.error) {
 			if (data.isEmpty) {
-				console.log('are empty');
 
+				// Add the label error class
 				for (var i = 0; i < data.errors.length; i++) {
 					$(`label[for="${data.errors[i].param}"]`).classList.add('label-error');
 				}
+
 			}else {
 				console.log('are not empty');
 			}
@@ -58,5 +51,5 @@ btn.addEventListener('click', (event) => {
 			console.log('Success');
 		}
 
-	}, ajaxObject => console.log(ajaxObject.readyState));
+	}, ajaxObject => console.log(ajaxObject.readyState));	
 });
