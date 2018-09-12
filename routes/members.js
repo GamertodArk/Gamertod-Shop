12b2aa10279423179d91ad3fbd340aeaa405aad9
Elvis Garcia <cavesagarcia@hotmail.com>
2018-09-12 03:39:15 -0430

    Profile routing done

--- routes/members.js
+++ routes/members.js
@@ -2,12 +2,16 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
 	if (req.user) {
-		
-		res.render('members_home', {title: req.user.username, username: req.user.username});
-		
+		res.render('members_home', {title: req.user.username, username: req.user.username});		
 	}else {
 		res.redirect('/users/login');
 	}
+});
+
+// Show the profile
+router.get('/profile/:username', (req, res) => {
+	console.log(req.params.username);
+	res.send('Hello World');
 });
 
 // Logout a user
